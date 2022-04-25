5c77f531165ed6d33fd1796d7195fd45597f7127
NowakArtur97 <nowart97@gmail.com>
2022-04-25 10:15:51 +0200

    Generate project using Spring Initializr

---
+++ src/main/kotlin/com/nowakartur/animedownloader/AnimedownloaderApplication.kt
@@ -0,0 +1,11 @@
+package com.nowakartur.animedownloader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnimedownloaderApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnimedownloaderApplication>(*args)
+}
