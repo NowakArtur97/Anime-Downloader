ccd5d7003e754225e25d0c868fbcbeb0fda1bbed
NowakArtur97 <nowart97@gmail.com>
2022-07-01 13:42:36 +0200

    Close tab with information about automated browser

--- src/main/kotlin/com/nowakartur/animedownloader/download/common/DownloadPage.kt
+++ src/main/kotlin/com/nowakartur/animedownloader/download/common/DownloadPage.kt
@@ -5,6 +5,8 @@ import org.openqa.selenium.remote.RemoteWebDriver
 interface DownloadPage {
 
     fun connectToDownloadPage(webDriver: RemoteWebDriver, url: String) {
+        webDriver.close()
+        webDriver.switchTo().window(webDriver.windowHandles.first())
         webDriver.get(url)
     }
 
